33d570dd070697e0a3afb9a62b1c32e6d76578e2
Kevin M <103332900+Reddimus@users.noreply.github.com>
2023-04-03 18:00:05 -0700

    Added comments

--- TrackExplorer_proj/Move.h
+++ TrackExplorer_proj/Move.h
@@ -1,11 +1,15 @@
 // Move.h
 // Runs on TM4C123
 // By Kevin Martinez
+/*
+Move file is a list of functions dedicated to all possible movements on a 2 motored car. 
+By changing directions and or turning on/ off 1 or 2 motors you get 9 possible movements.
+*/
 #include "Motors.h"
 
 #define WHEEL_DIR (*((volatile unsigned long *)0x400050F0)) // PB5432 are the four direction pins for L298
 
-// PB5432 are used for direction control on L298.
+// PB5432 are used for direction control on DRV8838.
 // Motor 1 is connected to the left wheel, Motor 2 is connected to the right wheel.
 #define FORWARD 0x28
 #define BACKWARD 0x3C
